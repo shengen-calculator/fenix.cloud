4da95320cec03909dc8bf9a596e5cfdba39d1456
Shengen <35036421+shengen-calculator@users.noreply.github.com>
2025-02-23 09:51:13 +0200

    Reconciliation (#2)
    * fix compiler error
    
    * fix report issue
    
    * small fix for xls filename
    
    * semicolon
    
    ---------
    
    Co-authored-by: bass <vasya_mail@ukr.net>

--- src/searchFunc.ts
+++ src/searchFunc.ts
@@ -1,9 +1,6 @@
-import {getReconciliationData} from "./main/getReconciliationData";
 import {getAnalogs} from "./search/getAnalogs";
 import {authDecorator} from "./authDecorator";
 import {ROLE} from "./role";
 
 exports.getAnalogs = authDecorator(getAnalogs,
     [ROLE.ADMIN, ROLE.MANAGER]);
-exports.getReconciliationData = authDecorator(getReconciliationData,
-    [ROLE.ADMIN, ROLE.MANAGER, ROLE.CLIENT])
